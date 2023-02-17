8c0263c60c1da11177fdff339ab8dfac5a8f945c
Aimar Karrik <71828910+AimarKarrik@users.noreply.github.com>
2023-02-17 09:31:03 +0200

    Created moviesController and moviesModel.

---
+++ backend/controllers/moviesController.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { MovieModel } from './../models/moviesModel';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    const page: number = parseInt(req.query.page as string);
+    const pageSize: number = parseInt(req.query.pageSize as string);
+
+    const pagedMovies =  MovieModel.getMovies(page, pageSize);
+    res.send(pagedMovies);
+});
+
+export default router;
