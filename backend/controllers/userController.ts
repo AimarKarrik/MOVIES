84ffc9105b2bc8f85203a1ef427d24d4ec212f32
Aimar Karrik <aimar.karrik@tptlive.ee>
2023-03-19 16:16:51 +0200

    user Basic Crud done

---
+++ backend/controllers/userController.ts
@@ -0,0 +1,98 @@
+import express from "express";
+import { getUserByEmail, createUser, deleteUser, updateUser } from "../services/userService";
+import User from "../models/userModel";
+import bcrypt from "bcrypt";
+import user from './../models/userModel';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const email: string = req.query.email as string;
+
+    const user: User | null = await getUserByEmail(email);
+    if (!user) return res.status(404).send("User not found");
+
+    const cleanUser = { ...user, password: undefined, id: undefined };
+
+    res.send(cleanUser);
+});
+
+
+router.post('/', async (req, res) => {
+    const userData: { email: string, password: string, name: string } = {
+        email: req.query.email as string,
+        password: req.query.password as string,
+        name: req.query.name as string
+    };
+
+    if (userData.email.length === 0 || userData.password.length === 0 || userData.name.length === 0) {
+        return res.status(400).send("Missing required fields");
+    }
+    if (!userData.email || !userData.password || !userData.name) {
+        return res.status(400).send("Missing required fields");
+    }
+
+    function validateEmail(email: string) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(email);
+    }
+
+    if (!validateEmail(userData.email)) {
+        return res.status(400).send("Invalid email");
+    }
+
+    const salt: string = await bcrypt.genSaltSync(10);
+    userData.password = await bcrypt.hash(userData.password, salt);
+
+    const user: User = await createUser(userData);
+
+    const cleanUser = { ...user, password: undefined, id: undefined };
+    return res.send(cleanUser);
+});
+
+
+router.delete('/', async (req, res) => {
+    const email: string = req.query.email as string;
+
+    const user: User | null = await deleteUser(email);
+    if (!user) return res.status(404).send("User not found");
+
+    const cleanUser = { ...user, password: undefined, id: undefined };
+    return res.send(cleanUser);
+});
+
+//untested
+router.put('/', async (req, res) => {
+    const userData: { email: string, password: string, name: string } = {
+        email: req.query.email as string,
+        password: req.query.password as string,
+        name: req.query.name as string
+    };
+
+    if (userData.email.length === 0 || userData.password.length === 0 || userData.name.length === 0) {
+        return res.status(400).send("Missing required fields");
+    }
+    if (!userData.email || !userData.password || !userData.name) {
+        return res.status(400).send("Missing required fields");
+    }
+
+    function validateEmail(email: string) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(email);
+    }
+
+    if (!validateEmail(userData.email)) {
+        return res.status(400).send("Invalid email");
+    }
+
+    const salt: string = await bcrypt.genSaltSync(10);
+    userData.password = await bcrypt.hash(userData.password, salt);
+
+    const updatedUser: User = await updateUser(userData);
+    if (!updatedUser) return res.status(404).send("User not found");
+
+    return res.send(updatedUser);
+});
+
+
+export default router;
---
+++ backend/services/userService.ts
@@ -0,0 +1,39 @@
+import prisma from "./prisma";
+import User from '../models/userModel';
+
+export async function getUserByEmail(email: string) {
+    const user: User | null = await prisma.users.findUnique({ where: { email: email } });
+
+    return user;
+}
+
+export async function createUser({ email, password, name }: { email: string, password: string, name: string }) {
+    const user: User = await prisma.users.create({
+        data: {
+            email: email,
+            password: password,
+            name: name
+        }
+    });
+
+    return user;
+}
+
+export async function deleteUser(email: string) {
+    const user: User | null = await prisma.users.delete({ where: { email: email } });
+
+    return user;
+}
+
+export async function updateUser({ email, password, name }: { email: string, password: string, name: string }) {
+    const user: User = await prisma.users.update({
+        where: { email: email },
+        data: {
+            email: email,
+            password: password,
+            name: name
+        }
+    });
+
+    return user;
+}
