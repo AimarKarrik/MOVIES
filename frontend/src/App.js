f4f5a9e15969e50b871fbd337ac92d5dc053d20c
gloriavimb <gloria.vimberg@tptlive.ee>
2023-04-05 20:34:01 +0300

    Changing reviews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./views/MainPage";
+import Reviews from "./components/Reviews";
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/Reviews" element={<Reviews />} />
       </Routes>
     </BrowserRouter>
   );
