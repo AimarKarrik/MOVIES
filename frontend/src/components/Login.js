ba35e3c8af5c560a70d10f0d460d3c0ac3b7db2c
gloriavimb <gloria.vimberg@tptlive.ee>
2023-04-09 13:44:14 +0300

    Made some improvements

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -6,11 +6,17 @@ export default function Login(props) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
+    const [formError, setFormError] = useState(null);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const storedEmail = localStorage.getItem('email');
         const storedPassword = localStorage.getItem('password');
+
+        if (!email || !password) {
+            setFormError("Please fill out all fields."); 
+            return; 
+        }
 
         if (email === storedEmail && password === storedPassword) {
             console.log('Logged in successfully!');
@@ -26,8 +32,11 @@ export default function Login(props) {
             <form className="login-form" onSubmit={handleSubmit}>
                 <label htmlFor="email"></label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Your email" id="email" name="email" />
+                {!email}
                 <label htmlFor="password"></label>
                 <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" id="password" name="password" />
+                {!password}
+                {formError && <p className="form-error">{formError}</p>}
                 <button type="submit">Log In</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Forgot password?</button>
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -8,9 +8,16 @@ export default function Signup(props) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
+    const [formError, setFormError] = useState(null);
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        if (!firstName || !lastName || !email || !password) {
+            setFormError("Please fill out all fields."); 
+            return; 
+        }
+
         localStorage.setItem('firstName', firstName);
         localStorage.setItem('lastName', lastName);
         localStorage.setItem('email', email);
@@ -20,25 +27,30 @@ export default function Signup(props) {
     };
 
     return (
-    <div className="Signup">
-        <div className="Signup-form-container">
-            <h1>Sign up</h1>
-            <form className="Signup-form" onSubmit={handleSubmit}>
-                <label htmlFor="first-name"></label>
-                <input value={firstName} onChange={(e) => setFirstName(e.target.value)} type="text" placeholder="First name" id="first-name" name="first-name" />
-                <label htmlFor="last-name"></label>
-                <input value={lastName} onChange={(e) => setLastName(e.target.value)} type="text" placeholder="Last name" id="last-name" name="last-name" />
-                <label htmlFor="email"></label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Your email" id="email" name="email" />
-                <label htmlFor="password"></label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" id="password" name="password" />
-                <button type="submit">Sign up</button>
-            </form>
-            <p>Do you have an account?</p>
-            <Link className="link" to="/Login">
-                <button className="link-btn-two" onClick={() => props.onFormSwitch('register')}>Log in</button>
-            </Link>
+        <div className="Signup">
+            <div className="Signup-form-container">
+                <h1>Sign up</h1>
+                <form className="Signup-form" onSubmit={handleSubmit}>
+                    <label htmlFor="first-name"></label>
+                    <input value={firstName} onChange={(e) => setFirstName(e.target.value)} type="text" placeholder="First name" id="first-name" name="first-name" />
+                    {!firstName}
+                    <label htmlFor="last-name"></label>
+                    <input value={lastName} onChange={(e) => setLastName(e.target.value)} type="text" placeholder="Last name" id="last-name" name="last-name" />
+                    {!lastName}
+                    <label htmlFor="email"></label>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Your email" id="email" name="email" />
+                    {!email}
+                    <label htmlFor="password"></label>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" id="password" name="password" />
+                    {!password}
+                    {formError && <p className="form-error">{formError}</p>}
+                    <button type="submit">Sign up</button>
+                </form>
+                <p>Do you have an account?</p>
+                <Link className="link" to="/Login">
+                    <button className="link-btn-two" onClick={() => props.onFormSwitch('register')}>Log in</button>
+                </Link>
+            </div>
         </div>
-    </div>
     )
 }
