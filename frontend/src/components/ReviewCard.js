3ddf7a5ef6de74401692d9e86806ba3ee44dfe8c
Aimar Karrik <71828910+AimarKarrik@users.noreply.github.com>
2023-04-12 18:16:20 +0300

    Update

--- frontend/src/components/ReviewCard.js
+++ frontend/src/components/ReviewCard.js
@@ -1,17 +1,28 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function ReviewCard({ review }) {
+  const [user, setUser] = useState({});
 
-    //get username from api
-
+  useEffect(() => {
+    fetch(`http://localhost:3001/users/ById?id=${review.userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data.user);
+      });
+  }, [review.userId]);
 
 
   return (
     <div className="review-card">
-        <div className="user">{review.userId}</div>
-        <div className="rating">{review.rating}</div>
         <h2>{review.title}</h2>
+        <p className="user">{user.name}</p>
+        <div className="rating">Rating: {review.rating}</div>
         <p>{review.content}</p>
     </div>
   );
